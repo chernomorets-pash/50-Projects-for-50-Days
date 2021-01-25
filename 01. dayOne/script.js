36c3111f92c6369157346122c42a1fec106389ad
Paul <p.chernomorets@gmail.com>
2021-01-25 12:12:36 +0200

    Expanding cards
    update day 1 - project one - Expanding cards

---
+++ 01. dayOne/script.js
@@ -0,0 +1,18 @@
+"use strict";
+
+//Создаем переменную панелей - которая привяжется ко всем елементам с классом 
+const panels = document.querySelectorAll('.panel')
+// console.log(panels)
+panels.forEach((panel) => {
+    // console.log(panel)
+    panel.addEventListener('click', () => {
+        removeActiveClasses()
+        panel.classList.add('active')
+    })
+})  
+
+function removeActiveClasses() {
+    panels.forEach((panel) => {
+        panel.classList.remove('active')
+    })
+}
